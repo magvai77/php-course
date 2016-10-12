b9d8436f7e084966a884cb7a6b2610b7803beff8
magvai77 <agdalon@gmail.com>
2016-10-12 21:14:39 +0700

    lection 5

--- myproject/index.php
+++ myproject/index.php
@@ -1,4 +1,27 @@
-		<?php require 'view/header.php'; ?>
+<?php
+$path = '';
+if (!empty($_GET['path'])) {
+	$path = $_GET['path'];
+}
+var_dump($_GET);
+$pathchunks = explode('-', $path);
+var_dump($pathchunks);
+$map = [
+	"loops" => [
+		"title" => "Loops",
+		"tasks" => ['1.php',2,3,4,5,6]
+	],
+	"arrays1" => [
+		"title" => "Arrays 1",
+		"tasks" => [1,2,3,4,5,6]
+	]
+];
+$section = $pathchunks[0];
+$tasks = $pathchunks[1];
+require __DIR__ . '/tasks/' . $section . '/' . $tasks . '.php'; 
+?>
+
+<?php require 'view/header.php'; ?>
 		<?php require 'view/menu.php'; ?>
 			<div class="workplace">
 				<h1>Loops -> 1</h1>
@@ -20,3 +43,8 @@
 				</div>
 			</div>
 		<?php require 'view/footer.php'; ?>
+
+<?php
+
+
+var_dump($map);
--- myproject/tasks/loops/1.php
+++ myproject/tasks/loops/1.php
@@ -8,26 +8,6 @@ function sayHello($count = 1)
 	return $result;
 }
 $result = sayHello(10);
-?>
-		<?php require '../../view/header.php'; ?>
-		<?php require '../../view/menu.php'; ?>
-			<div class="workplace">
-				<h1>Loops -> 1</h1>
-				<div class="task-item">
-					Task:<br>
-					Сказать 10 раз Hello World!
-				</div>
-				<div class="task-item">
-					Input:<br>
-					...
-				</div>
-				<div class="task-item">
-					Output:<br>
-					<?php echo $result; ?>
-				</div>
-				<div class="task-item">
-					Code:<br>
-					...
-				</div>
-			</div>
-		<?php require '../../view/footer.php'; ?>
+
+$description = '';
+$inputdata = '';
