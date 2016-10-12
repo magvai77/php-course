a99b03d73bdb185677f6bbb258d2ffee204020f4
magvai77 <agdalon@gmail.com>
2016-10-12 12:00:21 +0700

    Update menu.php

--- myproject/view/menu.php
+++ myproject/view/menu.php
@@ -2,24 +2,24 @@
 				<ul>
 					<li>Loop
 						<ol>
-							<li><a href="/loop1.php">1</a></li>
-							<li><a href="/loop2.php">2</a></li>
-							<li><a href="/loop3.php">3</a></li>
-							<li><a href="/loop4.php">4</a></li>
-							<li><a href="/loop5.php">5</a></li>
-							<li><a href="/loop6.php">6</a></li>
+							<li><a href="/tasks/loops/1.php">1</a></li>
+							<li><a href="/tasks/loops/2.php">2</a></li>
+							<li><a href="/tasks/loops/3.php">3</a></li>
+							<li><a href="/tasks/loops/4.php">4</a></li>
+							<li><a href="/tasks/loops/5.php">5</a></li>
+							<li><a href="/tasks/loops/6.php">6</a></li>
 						<ol/>
 					</li>
 					</ul>
 					<ul>
 					<li>Arrays - 1
 						<ol>
-							<li><a href="/array1.1.php">1</a></li>
-							<li><a href="/array1.2.php">2</a></li>
-							<li><a href="/array1.3.php">3</a></li>
-							<li><a href="/array1.4.php">4</a></li>
-							<li><a href="/array1.5.php">5</a></li>
-							<li><a href="/array1.6.php">6</a></li>
+							<li><a href="/tasks/array1/1.php">1</a></li>
+							<li><a href="/tasks/array1/2.php">2</a></li>
+							<li><a href="/tasks/array1/3.php">3</a></li>
+							<li><a href="/tasks/array1/4.php">4</a></li>
+							<li><a href="/tasks/array1/5.php">5</a></li>
+							<li><a href="/tasks/array1/6.php">6</a></li>
 							<li><a href="#">7</a></li>
 						</ol>			
 					</li>
