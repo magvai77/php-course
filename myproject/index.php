acbf187ded68980d623d003d61fc1842f9b5c7e0
magvai77 <agdalon@gmail.com>
2016-10-14 21:10:58 +0700

    lection 6

--- myproject/index.php
+++ myproject/index.php
@@ -1,9 +1,9 @@
 <?php
 $path = '';
+$flag = false;
 if (!empty($_GET['path'])) {
 	$path = $_GET['path'];
-
-	$pathchunks = explode('-', $path);
+	$flag = true;
 	$map = [
 		"loops" => [
 			"title" => "Loops",
@@ -34,20 +34,44 @@ if (!empty($_GET['path'])) {
 						"5.php",
 						"6.php"
 			]
+		],
+				"function" => [
+			"title" => "Function",
+			"tasks" => ["1.php",
+						"2.php",
+						"3.php",
+						"4.php",
+						"5.php"
+			]
 		]
 	];
-	var_dump($map);
+
+	$pathchunks = explode('-', $path);
+
 	$section = $pathchunks[0];
 	$tasks = $pathchunks[1];
 
+	$sectionData = $map[$section];
+	$taskName = $tasks;
+	$titlechunks = [
+		$sectionData['title'],
+		$taskName
+	];
+
+	$pagetitle = implode(' -> ', $titlechunks);
+
 	require __DIR__ . '/tasks/' . $section . '/' . $tasks . '.php';
+
 }
 ?>
 
-<?php require 'view/header.php'; ?>
-		<?php require 'view/menu.php'; ?>
-			<div class="workplace">
-				<h1>Loops</h1>
+
+<?php require 'view/header.php';
+require 'view/menu.php'; ?>
+	
+	<div class="workplace">
+ 		<?php if ($flag == true) { ?>
+				<h1> <?php echo $pagetitle ?></h1>
 				<div class="task-item">
 					Task:<br>
 					<?php echo $description ?>
@@ -64,6 +88,8 @@ if (!empty($_GET['path'])) {
 					Code:<br>
 				</div>
 			</div>
+			<?php  } else {?>
+			<img src="/html/images/pikachu.jpg">
+			<?php } ?>
 		<?php require 'view/footer.php'; ?>
 
-<?php
