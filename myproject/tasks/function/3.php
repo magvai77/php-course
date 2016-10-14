8ac743e41e594322cbca0cf31e2381e4f7b767fe
magvai77 <agdalon@gmail.com>
2016-10-14 16:13:36 +0700

    homework 1.5

--- myproject/tasks/function/3.php
+++ myproject/tasks/function/3.php
@@ -1,9 +1,9 @@
 <?php
-function percentCorrection($array,$percent)
+function percentCorrection($array, $percent)
 {
 	if ($percent >= 0) {
 		foreach ($array as $key => $value) {
-			$array[$key] = $value * ($percent > 2 ? ($percent -1) : $percent);
+			$array[$key] = $value * ($percent > 2 ? ($percent - 1) : $percent);
 		}
 		return $array;
 	} else {
@@ -12,5 +12,5 @@ function percentCorrection($array,$percent)
 	}
 }
 
-$result = percentCorrection([1,2,3,4,5,1.5,400,0.8,2.5],-3);
+$result = percentCorrection([1,2,3,4,5,1.5,400,0.8,2.5], 7);
 var_dump($result);
